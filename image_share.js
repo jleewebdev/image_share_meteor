87a73129cc5e70eed60105f92c69d2f7ee1ea6e4
jleewebdev <lee45276@gmail.com>
2016-02-28 19:38:59 +0800

    Added bootstrap

--- image_share.js
+++ image_share.js
@@ -1,7 +1,23 @@
 if (Meteor.isClient) {
-  console.log(" I am the client")
+  var img_data = [
+    {
+      img_src: "cat.png",
+      img_alt: "a cute cat"
+    },
+    {
+      img_src: "dog.jpg",
+      img_alt: "a nice dog"
+    },
+    {
+      img_src: "bird.jpg",
+      img_alt: "pretty bird"
+    },
+
+    ]
+
+  Template.images.helpers({images: img_data});
 }
 
 if (Meteor.isServer) {
-  console.log("I am the server"ßß)
+  console.log("I am the server");
 }
